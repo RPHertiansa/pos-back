e8c125973253d9da7d56b01060af3e07c52a7e57
RPHertiansa <reyhertiansa@gmail.com>
2020-10-30 04:30:27 +0700

    fix bug

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const categoryController = require('../controllers/category')
 const { authenticate, authorize } = require('../helpers/auth')
-const redisGate = require('../helpers/redis')
 
 const router = express.Router();
 
 router
-    .get('/getall', authenticate, authorize, redisGate.getCategory ,categoryController.getAll)
+    .get('/getall', authenticate, authorize, categoryController.getAll)
     .get('/getdetail/:id', authenticate, authorize, categoryController.getDetail)
     .post('/insert', authenticate, authorize, categoryController.insert)
     .put('/update/:id', authenticate, authorize, categoryController.update)
